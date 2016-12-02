907f7413012d94a33aaa07764b3cbc0bd2ba01ed
Chris Houdlette <choudlette@gmail.com>
2016-12-02 14:09:19 -0700

    connected up port stuff crt

---
+++ client/app.js
@@ -0,0 +1,7 @@
+(document).ready() {
+
+}
+
+$.get('http://localhose:3000/crunchy', (crunchy) => {
+  console.log("cdjfjdl")
+})
--- server/app.js
+++ server/app.js
@@ -1,8 +1,15 @@
 var crunchy = require('./crunchy.js');
+var express = require('express')
 var app = express();
+
+app.listen(3000, function() {
+  console.log("listening on port 3000!")
+})
 
 app.get('/crunchy', function(req,res,next){
   res.json(crunchy);
 });
+
+
 
 console.log(crunchy);
