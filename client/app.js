44199de2984b1497aa621c30e43d406cd3df17a6
Chris Houdlette <choudlette@gmail.com>
2016-12-02 14:40:31 -0700

    word is appending, materialize is in

--- client/app.js
+++ client/app.js
@@ -1,9 +1,15 @@
 $(document).ready(function(){
-  var cruncharray;
+  var crunchyarray;
+  var appendthingy;
   $.get('http://localhost:3000/crunchy', (crunchy) => {
     crunchyarray = crunchy;
 
   });
 
-  
+  $('button').on('click', function(event){
+    event.preventDefault();
+    var randomWord = crunchyarray[Math.floor(Math.random() * crunchyarray.length)];
+    var appendthingy = `<h1>${randomWord}</h1>`;
+     $('#appendhere').html(appendthingy);
+  });
 });
